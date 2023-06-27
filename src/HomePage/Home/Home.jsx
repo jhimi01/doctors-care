5050c811fdecdaef46fa1f50ae7ea0f7c6a50553
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-27 16:40:57 +0600

    responsive props

--- src/HomePage/Home/Home.jsx
+++ src/HomePage/Home/Home.jsx
@@ -1,18 +1,19 @@
-
-// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import bg from '../../assets/bgbannner.png';
-// import doctor from '../../assets/womendoctor.png';
-import doctor from '../../assets/doctor.png';
+import doctor from '../../assets/womendoctor.png';
+// import doctor from '../../assets/doctor.png';
 import AboutUs from '../AboutUs/AboutUs';
+import prop1 from '../../assets/Props/prop1.png'
+import prop10 from '../../assets/Props/prop10.png'
 
 const Home = () => {
   return (
    <>
 
 
-   {/* <Helmet>
+   <Helmet>
     <title>home | Doctor Care</title>
-   </Helmet> */}
+   </Helmet>
         <div className="hero h-[100vh] md:mt-0 mt-20" style={{ backgroundImage: `url(${bg})` }}>
       <div className="md:hero-content">
         <div className="md:w-1/2 w-full mx-auto md:space-y-3 space-y-1">
@@ -20,10 +21,12 @@ const Home = () => {
         <h2 className='md:text-5xl text-3xl font-bold text-[#1f2278]'>Protect Your Health And Take Care To Of Your Health !</h2>
         <button className='text-xl p-3 rounded-md bg-[#5559ce] text-white hover:bg-[#6d6fb8]'>Read More</button>
         </div>
-        <div className="md:w-1/2 h-full w-full mx-auto text-black">
-           <img className='w-[100%] h-full object-cover md:mt-10' src={doctor} alt="" />
+        <div className="md:w-[40%] w-full mx-auto text-black">
+           <img className='w-[100%] animate-updown object-cover md:mt-10' src={doctor} alt="" />
         </div>
       </div>
+      <div><img src={prop1} alt="prop"  className='hidden md:block animate-updown absolute left-5 bottom-8'/></div>
+      <div><img src={prop10} alt="prop"  className='animate-updown hidden md:block absolute top-36  left-[40%]'/></div>
     </div>
 
 
