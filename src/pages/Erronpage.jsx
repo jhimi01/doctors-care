5cdab587aac2edb92939f8177f0ac74367d2f3c9
Jhimi Talukder <talukderjhimi@gmail.com>
2023-06-30 23:24:32 +0600

    modify numbers

--- src/pages/Erronpage.jsx
+++ src/pages/Erronpage.jsx
@@ -3,8 +3,8 @@ import errorsvg from '../assets/error.svg'
 
 const Erronpage = () => {
     return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-           <img className='md:w-[35%] mx-auto' src={errorsvg} alt="" />
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100 ">
+           <img className='md:w-[30%] mx-auto animate-updown' src={errorsvg} alt="" />
       <h1 className="text-2xl font-bold text-[#407bff] mb-1">Oops!</h1>
       <p className="text-gray-700 text-xl mb-2">Something went wrong.</p>
      <Link to='/'> <button className="px-6 py-2 bg-[#407bff] text-white rounded hover:bg-[#5582e2]">
