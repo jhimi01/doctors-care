eb71d7673e246d030721ed1128f5dc232c5e2834
Jhimi Talukder <talukderjhimi@gmail.com>
2023-07-01 16:56:10 +0600

    done styling scroll

--- src/component/share/ScrollTop.jsx
+++ src/component/share/ScrollTop.jsx
@@ -1,30 +1,17 @@
 import React from 'react';
 import ScrollToTop from "react-scroll-to-top";
+import { BsArrowUpCircleFill } from 'react-icons/bs';
 
 const ScrollTop = () => {
-    const scrollButtonStyle = {
-        backgroundColor: '#0ff',
-        color: '#fff',
-        borderRadius: '50%',
-        width: '40px',
-        height: '40px',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'fixed',
-        bottom: '20px',
-        right: '20px',
-        zIndex: '9999',
-        cursor: 'pointer',
-      };
     return (
         <div className='bg-gray-700'>
-        <ScrollToTop smooth
-        viewBox="0 0 24 24"
-        svgPath="M9 19c-4.286 1.35-4.286-2.55-6-3m12 5v-3.5c0-1 .099-1.405-.5-2 2.791-.3 5.5-1.366 5.5-6.04a4.567 4.567 0 0 0 -1.333 -3.21 4.192 4.192 0 00-.08-3.227s-1.05-.3-3.476 1.267a12.334 12.334 0 0 0 -6.222 0C6.462 2.723 5.413 3.023 5.413 3.023a4.192 4.192 0 0 0 -.08 3.227A4.566 4.566 0 004 9.486c0 4.64 2.709 5.68 5.5 6.014-.591.589-.56 1.183-.5 2V21"
-    >
-        </ScrollToTop>
-      </div>
+      <ScrollToTop
+        smooth
+        component={<BsArrowUpCircleFill className='text-5xl text-[#f17732]'/>}
+        style={{ background: 'transparent' }}
+      >
+      </ScrollToTop>
+    </div>
     );
 };
 
