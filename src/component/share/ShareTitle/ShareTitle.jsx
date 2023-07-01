f2db336f45f168024fa021f2eab2592b1b1c187c
Jhimi Talukder <talukderjhimi@gmail.com>
2023-07-01 23:22:58 +0600

    search implement

--- src/component/share/ShareTitle/ShareTitle.jsx
+++ src/component/share/ShareTitle/ShareTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import bg from '../../../assets/bgbannner.png';
 const ShareTitle = ({title}) => {
     return (
-        <div className="hero h-[300px] text-indigo-500" style={{ backgroundImage: `url(${bg})` }}>
+        <div className="hero h-[250px] text-indigo-500" style={{ backgroundImage: `url(${bg})` }}>
          <div className="hero-overlay bg-opacity-10"></div>
             <h2 className='text-4xl font-bold'>{title}</h2>
         </div>
--- src/pages/Doctor/DoctorRoute.jsx
+++ src/pages/Doctor/DoctorRoute.jsx
@@ -3,54 +3,65 @@ import ShareTitle from "../../component/share/ShareTitle/ShareTitle";
 import useDoctors from "../../hooks/useDoctors";
 import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 import { AiFillPhone } from "react-icons/ai";
+import { FcSearch } from "react-icons/fc";
+import { useState } from "react";
 
 const DoctorRoute = () => {
   const { alldoctors } = useDoctors();
+  const [search, setSearch] = useState("");
 
   const openModal = (doctor) => {
     const modal = document.getElementById("my_modal_2");
-  modal.querySelector(".doctorname").textContent = doctor?.doctors?.name;
-  modal.querySelector(".doctor-specialty").textContent = `Specialty: ${doctor.serviceName}`;
-  modal.querySelector(".doctor-details").textContent = doctor.serviceDetails;
-  modal.querySelector(".doctor-mobile").textContent = `Mobile : ${doctor?.doctors?.mobile}`;
+    modal.querySelector(".doctorname").textContent = doctor?.doctors?.name;
+    modal.querySelector(".doctor-specialty").textContent = `Specialty: ${doctor.serviceName}`;
+    modal.querySelector(".doctor-details").textContent = doctor.serviceDetails;
+    modal.querySelector(".doctor-mobile").textContent = `Mobile : ${doctor?.doctors?.mobile}`;
 
-  // Clear existing social links
-  const socialLinksContainer = modal.querySelector(".social-links");
-  socialLinksContainer.innerHTML = "socials : ";
+    // Clear existing social links
+    const socialLinksContainer = modal.querySelector(".social-links");
+    socialLinksContainer.innerHTML = "socials : ";
 
-  // Add social links
-  doctor.socialLinks.forEach((socialLink) => {
-    const link = document.createElement("a");
-    link.href = socialLink.url;
-    link.target = "_blank"; // Add target="_blank" to open link in a new tab
+    // Add social links
+    doctor.socialLinks.forEach((socialLink) => {
+      const link = document.createElement("a");
+      link.href = socialLink.url;
+      link.target = "_blank"; // Add target="_blank" to open link in a new tab
 
+      // Create an icon element based on social platform
+      let iconElement;
+      switch (socialLink.platform) {
+        case "Facebook":
+          iconElement = `<FaFacebook />`;
+          break;
+        case "Twitter":
+          iconElement = `<FaTwitter />`;
+          break;
+        case "Instagram":
+          iconElement = `<FaInstagram />`;
+          break;
+        default:
+          iconElement = "";
+          break;
+      }
 
-    // Create an icon element based on social platform
-    let iconElement;
-    switch (socialLink.platform) {
-      case "Facebook":
-        iconElement = `<FaFacebook />`;
-        break;
-      case "Twitter":
-        iconElement = `<FaTwitter />`;
-        break;
-      case "Instagram":
-        iconElement = `<FaInstagram />`;
-        break;
-      default:
-        iconElement = "";
-        break;
-    }
+      // Set the text content as a combination of icon and platform
+      link.innerHTML = `${iconElement} ${socialLink.platform}`;
 
-    // Set the text content as a combination of icon and platform
-    link.innerHTML = `${iconElement} ${socialLink.platform}`;
+      socialLinksContainer.appendChild(link);
+    });
 
-    socialLinksContainer.appendChild(link);
-  });
-
-
-  window.my_modal_2.showModal();
+    window.my_modal_2.showModal();
   };
+
+  const handleSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredDoctors = alldoctors.filter(
+    (doctor) =>
+      doctor?.doctors?.name.toLowerCase().includes(search.toLowerCase()) ||
+      doctor?.serviceName.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
@@ -58,38 +69,61 @@ const DoctorRoute = () => {
         <title>doctors | Doctor Care</title>
       </Helmet>
       <ShareTitle title={"our docotors"}></ShareTitle>
-      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 w-full md:w-11/12 mt-10 mx-auto">
-        {alldoctors?.map((alldoctor, index) => (
+
+      {/* search */}
+      <div className="relative w-full md:w-1/2 mx-auto mt-4">
+        <input
+          onChange={handleSearch}
+          type="text"
+          placeholder="search by name or dissase"
+          className="input input-bordered w-full"
+        />
+        <button className="absolute right-3 top-3">
+          <FcSearch className="text-3xl" />
+        </button>
+      </div>
+
+      <div className="grid lg:grid-cols-4 md:grid-cols-3 grid-cols-1 gap-5 w-full md:w-11/12 mt-5 mx-auto">
+        {filteredDoctors?.map((alldoctor, index) => (
           <div key={index} className="card w-full bg-base-100 shadow-xl hoverEffect group">
             <figure className="px-5 pt-5">
-              <img src={alldoctor?.doctors?.image} alt="Shoes" className="rounded-xl h-[250px] w-full object-cover" />
+              <img
+                src={alldoctor?.doctors?.image}
+                alt="Shoes"
+                className="rounded-xl h-[250px] w-full object-cover"
+              />
             </figure>
             <div className="card-body items-center text-center">
-              <h2 className="card-title text-[#f17732] text-2xl group-hover:text-white">{alldoctor?.doctors?.name}</h2>
+              <h2 className="card-title text-[#f17732] text-2xl group-hover:text-white">
+                {alldoctor?.doctors?.name}
+              </h2>
               <p className="text-xl text-[#ce7541] group-hover:text-white">Specialist: {alldoctor?.serviceName}</p>
               <div className="card-actions">
-                <button onClick={() => openModal(alldoctor)} className="text-xl btn rounded-md bg-[#f17732] text-white hover:bg-[#e08c6800]">Detail</button>
+                <button
+                  onClick={() => openModal(alldoctor)}
+                  className="text-xl btn rounded-md bg-[#f17732] text-white hover:bg-[#e08c6800]"
+                >
+                  Detail
+                </button>
               </div>
             </div>
           </div>
         ))}
       </div>
       <dialog id="my_modal_2" className="modal">
-  <form method="dialog" className="modal-box bg-rose-50">
-    <h3 className="doctorname font-bold text-xl text-[#f17732]"></h3>
-    <p className="font-normal text-lg doctor-specialty"></p>
-    <p className="font-normal text-lg doctor-details"></p>
-    <p className="font-normal text-lg doctor-mobile"></p>
-    {/* Add more elements for other information */}
-    <div className="social-links"></div> {/* Add this container for social links */}
-    {/* <p className="py-4">Press ESC key or click outside to close</p> */}
-  </form>
-  <form method="dialog" className="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>
-
-
+        <form method="dialog" className="modal-box bg-rose-50">
+          <h3 className="doctorname font-bold text-xl text-[#f17732]"></h3>
+          <p className="font-normal text-lg doctor-specialty"></p>
+          <p className="font-normal text-lg doctor-details"></p>
+          <p className="font-normal text-lg doctor-mobile"></p>
+          {/* Add more elements for other information */}
+          <div className="social-links"></div> {/* Add this container for social links */}
+          {/* <p className="py-4">Press ESC key or click outside to close</p> */}
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
     </div>
   );
 };
